d0993d4af7c8e8b16e2e3bb60c940e33d8bbfbcd
Shreyojit <shreyojitdas95@gmail.com>
2025-05-07 03:24:31 +0530

    Initial commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "awscloudcore-hub"
+
+    workspaces {
+      name = "s3-uploader"
+    }
+  }
+  required_version = ">= 1.6.3"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
